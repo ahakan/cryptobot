507dbf7e68cc7576e4f59d300cc60ad6c1265cb7
Ahmet Hakan Çelik <ahmethcelik@gmail.com>
2022-03-11 14:07:22 +0300

    Added: Check cmake version in install.sh. Updated: Some library path in install.sh.

--- install.sh
+++ install.sh
@@ -2,37 +2,67 @@
 
 sudo rm -rf build
 
+CHECKCMAKE=$(ldconfig -p | cmake --system-information | grep CMAKE_VERSION | cut -c 16-21)
+
+CMAKEREQUIREDVER="3.50.0"
+
+CMAKEURL="https://github.com/Kitware/CMake.git"
+
+if [ "$(printf '%s\n' "$CMAKEREQUIREDVER" "$CHECKCMAKE" | sort -V | head -n1)" = "$CMAKEREQUIREDVER" ]; then 
+    git clone $CMAKEURL
+    cd CMake
+    ./bootstrap && make && sudo make install
+else
+    echo "-- Found CMAKE: TRUE"
+fi
+
 CMAKE_COMMAND="cmake .. "
 
 # Check libcap
-FINDLIBCAP=$(ldconfig -p | grep libcap)
+CHECKLIBCAP=$(ldconfig -p | grep /usr/lib/libcap)
+CHECKLOCALLIBCAP=$(ldconfig -p | grep /usr/local/libcap)
 
-if [ -n "$FINDLIBCAP" ]; then
+if [ -n "$CHECKLIBCAP" ]; then
     echo "-- Found Libcap: TRUE"
     CMAKE_COMMAND="${CMAKE_COMMAND} -DBUILD_LIBCAP=FALSE"
 else
-    echo "Libcap will be installed."
-    CMAKE_COMMAND="${CMAKE_COMMAND} -DBUILD_LIBCAP=TRUE"
+    if [ -n "$CHECKLOCALLIBCAP" ]; then
+        echo "-- Found Libcap: TRUE"
+        CMAKE_COMMAND="${CMAKE_COMMAND} -DBUILD_LIBCAP=FALSE"
+    else
+        echo "Libcap will be installed."
+        CMAKE_COMMAND="${CMAKE_COMMAND} -DBUILD_LIBCAP=TRUE"
+    fi
 fi
 
 # Check sqlite
-FINDSQLITE=$(ldconfig -p | grep libsqlite3)
+CHECKSQLITE=$(ldconfig -p | grep /usr/lib/libsqlite3)
+CHECKLOCALSQLITE=$(ldconfig -p | grep /usr/local/libsqlite3)
 
-if [ -n "$FINDSQLITE" ]; then
+if [ -n "$CHECKSQLITE" ]; then
     CMAKE_COMMAND="${CMAKE_COMMAND} -DBUILD_SQLITE3=FALSE"
 else
-    echo "LibSQLite3 will be installed."
-    CMAKE_COMMAND="${CMAKE_COMMAND} -DBUILD_SQLITE3=TRUE"
+    if [ -n "$CHECKLOCALSQLITE" ]; then
+        CMAKE_COMMAND="${CMAKE_COMMAND} -DBUILD_SQLITE3=FALSE"
+    else
+        echo "LibSQLite3 will be installed."
+        CMAKE_COMMAND="${CMAKE_COMMAND} -DBUILD_SQLITE3=TRUE"
+    fi
 fi
 
 # Check sqlite
-FINDCURL=$(ldconfig -p | grep curl)
+CHECKCURL=$(ldconfig -p | grep /usr/lib/libcurl)
+CHECKLOCALCURL=$(ldconfig -p | grep /usr/local/libcurl)
 
-if [ -n "$FINDCURL" ]; then
+if [ -n "$CHECKCURL" ]; then
     CMAKE_COMMAND="${CMAKE_COMMAND} -DBUILD_CURL=FALSE"
 else
-    echo "Libcurl will be installed."
-    CMAKE_COMMAND="${CMAKE_COMMAND} -DBUILD_CURL=TRUE"
+    if [ -n "$CHECKLOCALCURL" ]; then
+        CMAKE_COMMAND="${CMAKE_COMMAND} -DBUILD_CURL=FALSE"
+    else
+        echo "Libcurl will be installed."
+        CMAKE_COMMAND="${CMAKE_COMMAND} -DBUILD_CURL=TRUE"
+    fi
 fi
 
 mkdir build && cd build
